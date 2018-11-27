26fdd9e3b5ff1deae5e07d7b4f20bd0ff4ec717c
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-27 13:12:08 -0700

    more files

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+window.addEventListener("DOMContentLoaded", event => {
+	if(process.env.ENV === "live") {
+		enableProdMode();
+	}
+	const platform = platformBrowserDynamic();
+	platform.bootstrapModule(AppModule);
+});
